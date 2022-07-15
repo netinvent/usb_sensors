021d6b855b8d8c963162e3f1917983b3bb7e5d91
Orsiris de Jong <ozy@netpower.fr>
2022-07-15 16:43:48 +0200

    Add basic tests

---
+++ tests/test_usb_sensors.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+#  -*- coding: utf-8 -*-
+#
+# This file is part of usb_sensors module
+
+__intname__ = "usb_dogratian_sensors_tests"
+__author__ = 'Orsiris de Jong'
+__copyright__ = 'Copyright (C) 2022 Orsiris de Jong'
+__licence__ = 'BSD 3 Clause'
+__build__ = '2022071501'
+
+
+from time import sleep
+from usb_sensors import USBSensor
+
+
+def test_usb_sensors():
+    sensor_dict = USBSensor.find_sensors()
+    for sensor_type, sensor_ports in sensor_dict.items():
+        if not sensor_ports:
+            print("No {} sensors found".format(sensor_type))
+            continue
+        for sensor_port in sensor_ports:
+            print("Found sensor type {} at port {}".format(sensor_type, sensor_port))
+            sensor = USBSensor(sensor_port, read_light=True)
+
+            count = 0
+            try:
+                while count < 10:
+                    temperature = sensor.temperature
+                    humidity = sensor.humidity
+                    print("Current temperature: {} °C".format(temperature))
+                    print("Current humidity: {} %".format(humidity))
+                    assert isinstance(temperature, float), 'Temperature should be a float'
+                    assert isinstance(humidity, float), 'Humidity should be a float'
+                    sleep(.1)
+                    count += 1
+            except PermissionError as exc:
+                print("We don't have permission to use serial ports: {}".format(exc))
+
+
+if __name__ == "__main__":
+    test_usb_sensors()
