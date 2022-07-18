a58386668bfb8aef9bd54c2f513a62dd3f02d4df
Orsiris de Jong <ozy@netpower.fr>
2022-07-18 20:26:19 +0200

    Fix tests for updated class

--- tests/test_usb_sensors.py
+++ tests/test_usb_sensors.py
@@ -7,7 +7,7 @@ __intname__ = "usb_dogratian_sensors_tests"
 __author__ = 'Orsiris de Jong'
 __copyright__ = 'Copyright (C) 2022 Orsiris de Jong'
 __licence__ = 'BSD 3 Clause'
-__build__ = '2022071501'
+__build__ = '2022071801'
 
 
 from time import sleep
@@ -15,28 +15,26 @@ from usb_sensors import USBSensor
 
 
 def test_usb_sensors():
-    sensor_dict = USBSensor.find_sensors()
-    for sensor_type, sensor_ports in sensor_dict.items():
-        if not sensor_ports:
-            print("No {} sensors found".format(sensor_type))
-            continue
-        for sensor_port in sensor_ports:
-            print("Found sensor type {} at port {}".format(sensor_type, sensor_port))
-            sensor = USBSensor(sensor_port, read_light=True)
+    sensor_ports = USBSensor.find_sensors()
+    if not sensor_ports:
+        print("No sensors found")
+    for sensor_port in sensor_ports:
+        print("Found sensor at port {}".format(sensor_port))
+        sensor = USBSensor(sensor_port, read_light=True)
 
-            count = 0
-            try:
-                while count < 10:
-                    temperature = sensor.temperature
-                    humidity = sensor.humidity
-                    print("Current temperature: {} °C".format(temperature))
-                    print("Current humidity: {} %".format(humidity))
-                    assert isinstance(temperature, float), 'Temperature should be a float'
-                    assert isinstance(humidity, float), 'Humidity should be a float'
-                    sleep(.1)
-                    count += 1
-            except PermissionError as exc:
-                print("We don't have permission to use serial ports: {}".format(exc))
+        count = 0
+        try:
+            while count < 10:
+                temperature = sensor.temperature
+                humidity = sensor.humidity
+                print("Current temperature: {} °C".format(temperature))
+                print("Current humidity: {} %".format(humidity))
+                assert isinstance(temperature, float), 'Temperature should be a float'
+                assert isinstance(humidity, float), 'Humidity should be a float'
+                sleep(.1)
+                count += 1
+        except PermissionError as exc:
+            print("We don't have permission to use serial ports: {}".format(exc))
 
 
 if __name__ == "__main__":
